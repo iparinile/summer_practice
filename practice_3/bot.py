4e0fe1f4411e0cdcebb7b639c3993d1c582ada10
iparinile <poselennov.il@ya.ru>
2019-07-15 16:22:24 +0500

    test file

--- practice_3/bot.py
+++ practice_3/bot.py
@@ -5,12 +5,17 @@ bot = telebot.TeleBot("844950001:AAHeEy2Jn3V1uoIO5Lv8R-RUBcj2_IRptho")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Где деньги, Лебовски?")
 
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(content_types=["text"])
+def message(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+@bot.message_handler(content_types=["sticker"])
+def sticker(sticker):
+    bot.send_sticker(sticker.chat.id, sticker.sticker.file_id)
 
 
 bot.polling()
