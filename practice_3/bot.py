0503276f2e954ffe17f3bd82a74fdbab7dd6d2b0
iparinile <poselennov.il@ya.ru>
2019-07-18 13:17:11 +0500

    test file

--- practice_3/bot.py
+++ practice_3/bot.py
@@ -2,20 +2,34 @@ import telebot
 
 bot = telebot.TeleBot("844950001:AAHeEy2Jn3V1uoIO5Lv8R-RUBcj2_IRptho")
 
+keyboard = telebot.types.ReplyKeyboardMarkup(True, True)  # 1 true - уменьшает размер кнопок,
+# 2 - закрывает кнопки после нажатия
+keyboard.row('Привет', 'Пока')
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Где деньги, Лебовски?")
+    bot.send_message(message.chat.id, "Где деньги, Лебовски?", reply_markup=keyboard)
 
 
 @bot.message_handler(content_types=["text"])
 def message(message):
-    bot.send_message(message.chat.id, message.text)
+    if message.text.lower() == 'привет':  # Добавил функцию, теперь хоть 'ПрИВет' пиши, бот поймет
+        bot.send_message(message.chat.id, 'Привет, ' + message.from_user.first_name)
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Пока, ' + message.from_user.first_name)
+    else:
+        bot.send_message(message.chat.id, message.text)
 
 
 @bot.message_handler(content_types=["sticker"])
 def sticker(sticker):
     bot.send_sticker(sticker.chat.id, sticker.sticker.file_id)
+
+
+@bot.message_handler(content_types=["photo"])
+def photo(photo):
+    bot.send_photo(photo.chat.id, photo.photo.file_id)
 
 
 bot.polling()
